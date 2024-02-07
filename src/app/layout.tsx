b0b090cacf6db535b1111ed68b4c5f71ed0421b2
Paulo Ricardo <pauloricardo.m.almeida@gmail.com>
2024-02-07 04:45:10 -0300

    Feat(Home): init home page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,13 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Plus_Jakarta_Sans } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const plusJakartaSans = Plus_Jakarta_Sans({
+  variable: '--font-family-base',
+  subsets: ["latin"],
+  weight: ['300', '400', '600', '700', '800'],
+  style: ['normal'],
+});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -16,7 +21,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={plusJakartaSans.variable}>
+        {children}
+      </body>
     </html>
   );
 }
