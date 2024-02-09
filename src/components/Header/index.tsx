4334d4ca971962b7a46f26a2c130922d305f54c0
Paulo Ricardo <pauloricardo.m.almeida@gmail.com>
2024-02-09 01:27:12 -0300

    Feat(Header): finish Header with auth

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,38 +1,51 @@
+'use client'
+
+import useAuth, { TOKEN_KEY } from "@/hooks/useAuth";
 import Image from "next/image";
+import CircularButton from "../CircularButton";
 import styles from "./header.module.css";
 
-import logoHeader from "@/assets/images/LogoHeader.svg";
-import avatar from "@/assets/images/avatar.png";
-import CircularButton from "../CircularButton";
+const logoHeader = "/assets/images/LogoHeader.svg";
+const iconUser = "/assets/images/icon-user.svg";
+const avatar = "/assets/images/avatar.png";
 
-interface HeaderProps {
-  isLogged: boolean;
-}
-
-export default function Header({ isLogged }: HeaderProps) {
+export default function Header() {
+  const { isLogged, login, logout } = useAuth();
 
   return (
     <header className={styles.headerContainer}>
-      <div className={styles.headerLogoContainer}>
-        <Image src={logoHeader} alt="Logo" className={styles.headerLogoImage} />
-      </div>
-      <div className={styles.headerContent}>
-        <CircularButton>
-          {!isLogged && (
-            <Image
-              src={logoHeader}
-              alt="Logo"
-              className={styles.headerAvatar}
-            />
-          )}
-          {isLogged && (
-            <Image
-              src={avatar}
-              alt="Logo"
-              className={styles.headerAvatar}
-            />
-          )}
-        </CircularButton>
+      <div className={styles.headerFixedContainer}>
+        <div className={styles.headerLogoContainer}>
+          <Image
+            src={logoHeader}
+            alt="Logo"
+            width={120}
+            height={42}
+            className={styles.headerLogoImage}
+          />
+        </div>
+        <div className={styles.headerContent}>
+          <CircularButton onClick={isLogged ? logout : login}>
+            {!isLogged && (
+              <Image
+                src={iconUser}
+                alt="Logo"
+                width={28}
+                height={28}
+                className={styles.headerIconAvatar}
+              />
+            )}
+            {!!isLogged && (
+              <Image
+                src={avatar}
+                alt="Logo"
+                width={40}
+                height={40}
+                className={styles.headerAvatar}
+              />
+            )}
+          </CircularButton>
+        </div>
       </div>
     </header>
   )
