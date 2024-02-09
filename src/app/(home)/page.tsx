1dab3a6108f09390d1a641419a0f25c6234e6be5
Paulo Ricardo <pauloricardo.m.almeida@gmail.com>
2024-02-09 01:25:21 -0300

    Feat(App): config layout and home page ('/')

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,13 +1,19 @@
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
+import SectionTop from "./components/SectionTop";
+import SectionMid from "./components/SectionMid";
+import SectionBottom from "./components/SectionBottom";
 import styles from "./page.module.css";
 
 export default function Home() {
+
   return (
     <div className={styles.container}>
-      <Header isLogged />
+      <Header />
       <main className={styles.main}>
-        Hello World Igma!
+        <SectionTop />
+        <SectionMid />
+        <SectionBottom />
       </main>
       <Footer />
     </div>
